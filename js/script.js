cac68b460c37f53cebc0429645afc90335c566f1
kloepoitrot <KloePoitrot@hotmail.com>
2023-11-02 17:43:17 +0100

    Add images and text changes

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ let p1Move = "" // rock, paper, soissors
 let p2Move = "" // rock, paper, scissors
 let playerOrComp = "Computer" // Player 2, Computer
 let playerTurn = 0 // 0 = p1, 1 = p2, 2 = result
+let isGameFinished = false
 
 
 // ===============================================
@@ -29,16 +30,14 @@ vsComputer.addEventListener('click', () =>{
     playerOrComp = "Computer"
     
     console.log("comp")
-    if(p1 == 0 && p2 == 0){
+    if(p1 > 0 && p2 > 0){
         p1 = 0
         p2 = 0
         document.querySelector("#p1img").src = "images/waiting.png"
         document.querySelector("#p2img").src = "images/waiting.png"
-        document.querySelector("#p1img").classList.add("fa-pulse")
-        document.querySelector("#p2img").classList.add("fa-pulse")
     }
 
-    choiceP1(p1Move, p2Move, p1, p2, playerOrComp)
+    isGameFinished = false
 })
 
 vsPlayer.addEventListener('click', () =>{
@@ -51,7 +50,7 @@ vsPlayer.addEventListener('click', () =>{
     playerOrComp = "Player 2"
 
     console.log("p2")
-    if(p1 == 0 && p2 == 0){
+    if(p1 > 0 && p2 > 0){
         p1 = 0
         p2 = 0
         document.querySelector("#p1img").src = "images/waiting.png"
@@ -59,8 +58,8 @@ vsPlayer.addEventListener('click', () =>{
         document.querySelector("#p1img").classList.add("fa-pulse")
         document.querySelector("#p2img").classList.add("fa-pulse")
     }
-
-    choiceP1(p1Move, p2Move, p1, p2, playerOrComp)
+    
+    isGameFinished = false
 })
 
 
@@ -81,6 +80,8 @@ let scissors = document.querySelector("#scissors")
 rock.addEventListener("click", (e) => playRock())
 paper.addEventListener("click", (e) => playPaper())
 scissors.addEventListener("click", (e) => playScissors())
+
+
 
 function playRock(){
     if(playerTurn === 0){
@@ -153,20 +154,32 @@ function playScissors(){
 // ===============================================
 // ===============================================
 function playRound(){
+    p1Move == "rock" ? document.querySelector("#p1img").src = "images/rock.png" : null
+    p1Move == "paper" ? document.querySelector("#p1img").src = "images/paper.png" : null
+    p1Move == "scissors" ? document.querySelector("#p1img").src = "images/scissors.png" : null
+    
+    p2Move == "rock" ? document.querySelector("#p2img").src = "images/rock.png" : null
+    p2Move == "paper" ? document.querySelector("#p2img").src = "images/paper.png" : null
+    p2Move == "scissors" ? document.querySelector("#p2img").src = "images/scissors.png" : null
+
     // draw
     if((p1Move == "rock" && p2Move == "rock") || (p1Move == "paper" && p2Move == "paper") || (p1Move == "scissors" && p2Move == "scissors")){
         p1Move = ""
         p2Move = ""
         document.querySelector("#winner").innerHTML = "Its a draw!"
+        document.querySelector("#player1points").innerHTML = p1
+        document.querySelector("#player2points").innerHTML = p2
         console.log('draw')
     }
 
     // p1 won
     if((p1Move == "rock" && p2Move == "scissors") || (p1Move == "paper" && p2Move == "rock") || (p1Move == "scissors" && p2Move == "paper")){
-        p1Move += 1
+        p1 += 1
         p1Move = ""
         p2Move = ""
         document.querySelector("#winner").innerHTML = "Player 1 won this round!"
+        document.querySelector("#player1points").innerHTML = p1
+        document.querySelector("#player2points").innerHTML = p2
         console.log('p1 win')
     }
 
@@ -175,7 +188,9 @@ function playRound(){
         p2 += 1
         p1Move = ""
         p2Move = ""
-        document.querySelector("#winner").innerHTML = playerOrComp + "won this round!"
+        document.querySelector("#winner").innerHTML = playerOrComp + " won this round!"
+        document.querySelector("#player1points").innerHTML = p1
+        document.querySelector("#player2points").innerHTML = p2
 
         console.log('p2 win')
     }
@@ -186,5 +201,11 @@ function playRound(){
 
     if(p1 >= 10 || p2 >= 10){
         console.log('finished')
+        let winner = p1 >= 10 ? "Player 1" : playerOrComp
+        let winCrown = p1 >= 10 ? "#p1win" : "#p2win"
+
+        document.querySelector(winCrown).classList.add("fa-crown")
+        document.querySelector("#winner").innerHTML = winner + " is the winner!"
+        isGameFinished = true
     }
 }
